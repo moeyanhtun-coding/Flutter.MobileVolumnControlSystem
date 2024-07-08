18f6d99d059e189a73a3114cc0bd8b1eec06c0b5
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-08 13:52:46 +0630

    [Complete] Project Setup

---
+++ android/app/src/main/kotlin/com/example/volumn_system_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.volumn_system_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:volumn_system_app/volumn_system.dart';
+
+void main() {
+  runApp(const volumnSystem());
+}
